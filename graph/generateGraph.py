6555b81d8e4b848f8909ad9766892e0eec47a7a4
Charybdis <90869683+Charybdis2@users.noreply.github.com>
2025-07-15 03:33:42 +0100

    #4 added golden markers for pbs

--- graph/generateGraph.py
+++ graph/generateGraph.py
@@ -6,15 +6,28 @@ MINUTE = 60
 def generateGraph(c):
   x = []
   y = []
+  XNoPB = []
+  YNoPB = []
   for attempt in c.attempts:
     if attempt.attemptTime == None:
       continue
     else:
-      x.append(int(attempt.attemptNumber))
-      y.append(attempt.attemptTime.total_seconds()/MINUTE)
-    
 
-  plt.plot(x, y, marker='x')
+      xValue = int(attempt.attemptNumber)
+      yValue = attempt.attemptTime.total_seconds()/MINUTE
+      x.append(xValue)
+      y.append(yValue)
+      if attempt.isPb:
+        pbX = xValue
+        pbY = yValue 
+      else:
+        XNoPB.append(xValue)
+        YNoPB.append(yValue)
+
+  if pbX is not None and pbY is not None:
+    plt.scatter(pbX, pbY, color='gold', marker='x')   
+  plt.scatter(XNoPB, YNoPB, color='blue', marker='x')
+  plt.plot(x, y)
   plt.xlabel("Attempt Number")
   plt.ylabel("Time")
   plt.title(f"{c.gameName}")
@@ -27,16 +40,29 @@ def generateSplitGraph(c):
   for i in range(numberOfSplits):
     x = []
     y = []
+    XNoPB = []
+    YNoPB = []
     for attempt in c.attempts:
       if attempt.attemptNumber and len(attempt.splits) > i:
         split = attempt.splits[i]
         if split.splitTime:
+          xValue = int(attempt.attemptNumber)
+          yValue = split.splitTime.total_seconds()/MINUTE
+          if split.isPb:
+            pbX = xValue
+            pbY = yValue
+          else:
+            XNoPB.append(xValue)
+            YNoPB.append(yValue)
           x.append(int(attempt.attemptNumber))
           y.append(split.splitTime.total_seconds()/MINUTE)
     if not x:
       continue
 
-    plt.plot(x, y, marker='x')
+    if pbX is not None and pbY is not None:
+      plt.scatter(pbX, pbY, color='gold', marker='x')   
+    plt.scatter(XNoPB, YNoPB, color='blue', marker='x')
+    plt.plot(x, y)
     plt.xlabel("Attempt Number")
     plt.ylabel("Time")
     plt.title(f"{c.splitNames[i]}")
